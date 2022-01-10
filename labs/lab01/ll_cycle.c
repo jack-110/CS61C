527397411bee56e5baa903485a1c817cc620709d
陈洋 <13990779978@163.com>
2022-01-10 17:25:06 +0800

    exe 5 done, and lab01 done

--- labs/lab01/ll_cycle.c
+++ labs/lab01/ll_cycle.c
@@ -2,6 +2,15 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    node *p1 = head;
+    node *p2 = head;
+    while (p1 != NULL && p1->next != NULL) {
+    	p1 = p1->next->next;
+	p2 = p2->next;
+
+	if (p1 == p2) {
+	    return 1;
+	}
+    }
     return 0;
 }
