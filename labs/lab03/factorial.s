8792d622b469d543ff589fe1bbd430044bb5687d
陈洋 <13990779978@163.com>
2022-01-27 20:48:19 +0800

    lab03 ex03 done

--- labs/lab03/factorial.s
+++ labs/lab03/factorial.s
@@ -22,3 +22,20 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    add t0, a0, x0
+    beq t0, x0, exit
+    addi a0, a0, -1
+    addi sp, sp, -8
+    sw t0, 0(sp)
+    sw ra, 4(sp)
+    jal factorial
+    lw t0, 0(sp)
+    lw ra, 4(sp)
+    addi sp, sp, 8
+    add t1, a0, x0
+    mul a0, t0, t1
+    jr ra
+exit:
+    addi a0, x0, 1
+    add a1, x0, x0
+    jr ra
